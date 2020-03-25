f5cdd29d2b62c133b813a0ea99a32c2e8c1fd764
Marco Luchi <marcoaluchi@hotmail.com>
2020-03-25 16:43:17 -0300

    cambios menores, cambio de variables

--- js/apis.js
+++ js/apis.js
@@ -1,17 +1,14 @@
-var mapa; // Mapa que vamos a modificar
-var posicionCentral = {lat: 40.6643, lng: -73.9385};
-/* Crear la variable posicionCentral con las coordenadas donde se va a centrar el mapa */
+var mapa; // Mapa que vamos a modificar {lat: 40.6643, lng: -73.9385};
 
+
+/* Crear la variable posicionCentral con las coordenadas donde se va a centrar el mapa */
 // Inicializa el mapa con un valor de zoom y una locación en el medio
 function inicializarMapa () {
+  var posicionCentral = new google.maps.LatLng(40.6643,-73.9385);
     mapa = new google.maps.Map(document.getElementById('map'),{
-
-      center: {
-
-          lat: posicionCentral.lat,
-          lng: posicionCentral.lng
-      },
       zoom: 12,
+      center: posicionCentral,
+      
     })
     /* Modificá la variable mapa con el constructor Map().
     Tendrás que asignarle un valor de zoom y
