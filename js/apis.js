939f6d9993653918e348807c67d58392a01f3c33
Marco Luchi <marcoaluchi@hotmail.com>
2020-03-25 12:32:09 -0300

    se inicializo el mapa apuntando a NY

--- js/apis.js
+++ js/apis.js
@@ -1,9 +1,18 @@
-var mapa // Mapa que vamos a modificar
-
+var mapa; // Mapa que vamos a modificar
+var posicionCentral = {lat: 40.6643, lng: -73.9385};
 /* Crear la variable posicionCentral con las coordenadas donde se va a centrar el mapa */
 
 // Inicializa el mapa con un valor de zoom y una locación en el medio
 function inicializarMapa () {
+    mapa = new google.maps.Map(document.getElementById('map'),{
+
+      center: {
+
+          lat: posicionCentral.lat,
+          lng: posicionCentral.lng
+      },
+      zoom: 12,
+    })
     /* Modificá la variable mapa con el constructor Map().
     Tendrás que asignarle un valor de zoom y
     un centro igual a la variable posicionCentral. */
