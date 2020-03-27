5718adcb2096cb4718599991fd90149d98912c6b
Marco Luchi <marcoaluchi@hotmail.com>
2020-03-27 18:22:17 -0300

    se agrego funcionalidad street view correctamente

--- js/streetView.js
+++ js/streetView.js
@@ -1,14 +1,26 @@
 streetViewModulo = (function () {
-  var paronama // 'Visor' de StreetView
+  var panorama // 'Visor' de StreetView
 
   function inicializar () {
         /* Completar la función inicializar()  que crea un panorama
         en una posición y lo muestra en la página. */
+      var etiquetaSView = document.getElementById('pano');    
+      panorama = new google.maps.StreetViewPanorama(etiquetaSView,{
 
+        position: mapa.center,
+        pov: {
+          heading:150,
+          pitch: 10
+        }
+      });
   }
 
     // Actualiza la ubicación del Panorama
   function fijarStreetView (ubicacion) {
+      
+    //  panorama = mapa.getStreetView();
+      panorama.setPosition(ubicacion);
+      mapa.setStreetView(panorama);
         /* Completar la función fijarStreetView que actualiza la posición
          de la variable panorama y cambia el mapa de modo tal que se vea
          el streetView de la posición actual. */
