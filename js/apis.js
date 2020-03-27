b2b0790d463bc647997f780d0918e1ac636c017c
Marco Luchi <marcoaluchi@hotmail.com>
2020-03-27 14:29:13 -0300

    se cambio la ubicacion a NY mejor centrado

--- js/apis.js
+++ js/apis.js
@@ -2,7 +2,7 @@ var mapa; // Mapa que vamos a modificar {lat: 40.6643, lng: -73.9385};
 /* Crear la variable posicionCentral con las coordenadas donde se va a centrar el mapa */
 // Inicializa el mapa con un valor de zoom y una locación en el medio
 function inicializarMapa () {
-  var posicionCentral = new google.maps.LatLng(40.6643,-73.9385);
+  var posicionCentral = new google.maps.LatLng(40.76708822241082,-73.96836907958985);
     mapa = new google.maps.Map(document.getElementById('map'),{
       zoom: 12,
       center: posicionCentral,
